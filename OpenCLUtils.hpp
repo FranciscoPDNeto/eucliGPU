eebdf02938351eec4a8b861507e32b7fdd842816
Alexandre Cesar <acmorcego@gmail.com>
2019-11-17 00:54:18 -0300

    format code

--- OpenCLUtils.hpp
+++ OpenCLUtils.hpp
@@ -6,13 +6,13 @@
 
 namespace OpenCLUtils {
 
-
 std::vector<cl::Device> getDevices() {
   std::vector<cl::Platform> platforms;
   cl::Platform::get(&platforms);
 
   if (platforms.size() == 0) {
-    throw std::runtime_error("There is no platforms available. Check OpenCL installation!");
+    throw std::runtime_error(
+        "There is no platforms available. Check OpenCL installation!");
   }
 
   cl::Platform defaultPlatform = platforms[1];
@@ -22,7 +22,8 @@ std::vector<cl::Device> getDevices() {
   std::vector<cl::Device> devices;
   defaultPlatform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
   if (devices.size() == 0) {
-    throw std::runtime_error("There is no devices available. Check OpenCL installation!");
+    throw std::runtime_error(
+        "There is no devices available. Check OpenCL installation!");
   }
 
   return devices;
@@ -32,8 +33,9 @@ cl::Device getDevice(int deviceId) {
   std::vector<cl::Device> devices(getDevices());
 
   if (deviceId > static_cast<int>(devices.size())) {
-    throw std::runtime_error("There is just " + std::to_string(devices.size()) + 
-      " devices available, so there is no device id " + std::to_string(deviceId));
+    throw std::runtime_error("There is just " + std::to_string(devices.size()) +
+                             " devices available, so there is no device id " +
+                             std::to_string(deviceId));
   }
 
   const cl::Device device = devices[deviceId];
@@ -45,40 +47,47 @@ cl::Device userSelectDevice() {
   std::vector<cl::Device> devices(getDevices());
 
   for (std::size_t i = 0; i < devices.size(); ++i) {
-    std::cout << "Device " << i << " - " << devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
+    std::cout << "Device " << i << " - " << devices[i].getInfo<CL_DEVICE_NAME>()
+              << std::endl;
   }
 
   std::cout << "Select your device id: ";
   int deviceId;
   std::cin >> deviceId;
   cl::Device defaultDevice = devices[deviceId];
-  std::cout << "Using device: " << defaultDevice.getInfo<CL_DEVICE_NAME>() << "\n";
+  std::cout << "Using device: " << defaultDevice.getInfo<CL_DEVICE_NAME>()
+            << "\n";
 
   return defaultDevice;
 }
 
-void executeOpenCL(const std::string& kernelName, const std::string& kernelSource, const unsigned char *dataInput, 
-  const int dataInputSize, unsigned char *dataOutput, const int dataOutputSize) {
+void executeOpenCL(const std::string &kernelName,
+                   const std::string &kernelSource,
+                   const unsigned char *dataInput, const int dataInputSize,
+                   unsigned char *dataOutput, const int dataOutputSize) {
 
   const cl::Device defaultDevice = getDevice(0);
 
-  cl::Context context({ defaultDevice });
+  cl::Context context({defaultDevice});
   cl::Program::Sources sources;
-  
-  sources.push_back({ kernelSource.c_str(), kernelSource.length() });
+
+  sources.push_back({kernelSource.c_str(), kernelSource.length()});
   cl::Program program(context, sources);
-  if (program.build({ defaultDevice }) != CL_SUCCESS) {
-    throw std::runtime_error("Error building: "
-      + program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(defaultDevice));
+  if (program.build({defaultDevice}) != CL_SUCCESS) {
+    throw std::runtime_error(
+        "Error building: " +
+        program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(defaultDevice));
   }
 
-  cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * dataInputSize, nullptr);
-  cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * dataOutputSize, nullptr);
-  
+  cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR,
+                         sizeof(unsigned char) * dataInputSize, nullptr);
+  cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR,
+                          sizeof(unsigned char) * dataOutputSize, nullptr);
+
   cl::CommandQueue queue(context, defaultDevice);
-  queue.enqueueWriteBuffer(
-    inputBuffer, CL_TRUE, 0, sizeof(unsigned char) * dataInputSize, dataInput);
-  
+  queue.enqueueWriteBuffer(inputBuffer, CL_TRUE, 0,
+                           sizeof(unsigned char) * dataInputSize, dataInput);
+
   cl::Kernel kernel(program, kernelName.c_str());
   kernel.setArg(0, inputBuffer);
   kernel.setArg(1, outputBuffer);
@@ -86,8 +95,8 @@ void executeOpenCL(const std::string& kernelName, const std::string& kernelSourc
   queue.enqueueNDRangeKernel(kernel, 0, dataInputSize, 32);
 
   // Retorna o resultado da computação na GPU para o dataOutput.
-  queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, sizeof(unsigned char) * dataOutputSize, dataOutput);
+  queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0,
+                          sizeof(unsigned char) * dataOutputSize, dataOutput);
 }
-
 
 } // namespace OpenCLUtils
--- eucligpu.cpp
+++ eucligpu.cpp
@@ -1,36 +1,38 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 #define STB_IMAGE_IMPLEMENTATION
+#include "OpenCLUtils.hpp"
 #include "stb_image.h"
 #include "stb_image_write.h"
-#include "OpenCLUtils.hpp"
 
 #define KERNELNAME "euclidean"
 
 class ExecuteOpenCL {
 public:
-
-  ExecuteOpenCL(const std::string& filename) : m_filename(filename), m_image(nullptr), m_output(nullptr) {
-  };
+  ExecuteOpenCL(const std::string &filename)
+      : m_filename(filename), m_image(nullptr), m_output(nullptr){};
 
   void execute() {
     int imageWidth, imageHeight, imageChannels;
-    m_image = stbi_load(m_filename.c_str(), &imageWidth, &imageHeight, &imageChannels, 0);
+    m_image = stbi_load(m_filename.c_str(), &imageWidth, &imageHeight,
+                        &imageChannels, 0);
     if (m_image == nullptr)
-      throw std::runtime_error(
-        "The image could not be loaded, please check if the filename is corrected");
+      throw std::runtime_error("The image could not be loaded, please check if "
+                               "the filename is corrected");
 
     const int imageSize = imageWidth * imageHeight * imageChannels;
     m_output = new unsigned char[imageSize];
     assert(m_output != nullptr);
 
-    OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteOpenCL::readKernel(), m_image, imageSize, m_output, imageSize);
+    OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteOpenCL::readKernel(), m_image,
+                               imageSize, m_output, imageSize);
 
     for (int i = 0; i < imageSize; ++i) {
       if (static_cast<int>(m_output[i]) != 2) {
         throw std::runtime_error("Verification failed! result #2 , " +
-          std::to_string(static_cast<int>(m_output[i])) + " != 2 (expected).");
+                                 std::to_string(static_cast<int>(m_output[i])) +
+                                 " != 2 (expected).");
       }
     }
 
@@ -40,10 +42,9 @@ public:
   ~ExecuteOpenCL() {
     if (m_image != nullptr)
       stbi_image_free(m_image);
-    
+
     if (m_output != nullptr)
       free(m_output);
-
   };
 
 private:
@@ -57,25 +58,27 @@ private:
     input.seekg(0, std::ios::end);
     source.reserve(input.tellg());
     input.seekg(0, std::ios::beg);
-    source.assign((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+    source.assign((std::istreambuf_iterator<char>(input)),
+                  std::istreambuf_iterator<char>());
     return source;
   }
 };
 
-
 int main(int argc, char const *argv[]) {
   if (argc < 2) {
-      std::cerr << "You have to pass 1 argument to the program, but none was passed." << std::endl;
-      return -1;
+    std::cerr
+        << "You have to pass 1 argument to the program, but none was passed."
+        << std::endl;
+    return -1;
   }
 
   const std::string filename(argv[1]);
-  
+
   try {
     // Executa com o destrutor seguro para desalocar todos os ponteiros criados.
     ExecuteOpenCL exec(filename);
     exec.execute();
-  } catch (const std::runtime_error& e) {
+  } catch (const std::runtime_error &e) {
     throw e;
   }
 
