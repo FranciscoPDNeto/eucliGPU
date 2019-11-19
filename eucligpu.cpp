bdf5e78c5cd84ce2a76d68f89a8d963442c63844
Francisco Dias <franciscopdneto@gmail.com>
2019-11-19 19:10:13 -0300

    including strutures required to gpu calculation.

--- eucligpu.cpp
+++ eucligpu.cpp
@@ -9,19 +9,168 @@
 
 #define KERNELNAME "euclidean"
 
-void computeDistanceTransformImage(const unsigned char *imageInput, float *imageOutput,
-  const int imageWidth, const int imageHeight) {
+/**
+ * \brief Representação de coordenada 2d.
+*/
+typedef struct {
+  cl_uint index;
+  cl_uint y;
+  cl_uint x;
+
+} Coordinate;
+
+Coordinate constructCoord(unsigned int y, unsigned int x, unsigned int imageWidth) {
+  Coordinate coord;
+  coord.index = y*imageWidth + x;
+  coord.y = y;
+  coord.x = x;
+
+  return coord;
+}
+
+/**
+ * \brief Calcula a distância euclideana.
+*/
+cl_float euclideanDistance(const Coordinate& coord1, const Coordinate& coord2) {
+  return std::sqrt(std::pow(coord1.y - coord2.y, 2) + std::pow(coord1.x - coord2.x, 2));
+}
+
+/**
+ * \brief Representa o Pixel da imagem.
+*/
+typedef struct {
+  Coordinate coord;
+  // Valor que corresponde a true caso seja célula de fundo, ou false(0) caso seja célula de frente(1).
+  bool background;
+} Pixel;
+
+Pixel constructPixel(const Coordinate coord, const bool background) {
+  Pixel pixel;
+  pixel.coord = coord;
+  pixel.background = background;
+  
+  return pixel;
+}
+
+typedef struct {
+  // Há no máximo 8 vizinhos.
+  Pixel pixels[8];
+  // É unsigned char pois pode representar no máximo 8, então não é necessário
+  // mais de um byte.
+  cl_uchar size;
+  
+} Neighborhood;
+
+Neighborhood initNeighborhood() {
+  Neighborhood neighborhood;
+  neighborhood.size = 0;
+
+  return neighborhood;
+}
+
+void addNeighbor(Neighborhood neighborhood, Pixel pixel) {
+  neighborhood.pixels[neighborhood.size++] = pixel;
+}
+
+Pixel getNeighbor(const Neighborhood neighborhood, const unsigned char index) {
+  assert(index < neighborhood.size);
+  const Pixel pixel = neighborhood.pixels[index];
+
+  return pixel;
+}
+
+typedef struct {
+  cl_uint height;
+  cl_uint width;
+  cl_uint size;
+} ImageAttrs;
+
+typedef struct {
+  ImageAttrs attrs; 
+  cl_uchar *image;
+} UCImage;
+
+
+UCImage construcUCImage(unsigned char *image, const unsigned int height, const unsigned int width) {
+  UCImage ucimage;
+  ucimage.image = image;
+  ucimage.attrs.height = height;
+  ucimage.attrs.width = width;
+  ucimage.attrs.size = width * height;
+
+  return ucimage;
+}
+
+cl_uchar getValueByCoord(const UCImage *image, const Coordinate coord) {
+  return image->image[coord.y * image->attrs.width + coord.x];
+}
+
+bool isBackgroudByCoord(const UCImage *image, const Coordinate coord) {
+  return getValueByCoord(image, coord) == 0;
+}
+
+Pixel getPixel(const UCImage *image, cl_int y, cl_int x) {
+  const Coordinate coord = constructCoord(y, x, image->attrs.width);
+  return constructPixel(coord, isBackgroudByCoord(image, coord));
+}
+
+/**
+ * \brief Retorna a lista de valores vizinhos ao pixel na imagem.
+ * A vizinhança utilizada é a simples, vizinhança direta na janela 3x3.
+*/
+Neighborhood getNeighborhood(const UCImage *image, const Pixel& pixel) {
+  Neighborhood neighborhood;
+  for (cl_int i = -1; i < 2; i++) {
+    const cl_uint y = pixel.coord.y - i;
+    if (y >= image->attrs.height)
+      continue;
+
+    for (cl_int j = -1; i < 2; i++) {
+      const cl_uint x = pixel.coord.x - j;
+      if (x < 0 || x >= image->attrs.width)
+        continue;
+
+
+      addNeighbor(neighborhood, getPixel(image, y, x));
+    }
+  }
+  
+  return neighborhood;
+}
+
+typedef struct {
+  Coordinate point;
+  Coordinate nearestBackground;
+} VoronoiDiagramMapEntry;
+
+typedef struct {
+  VoronoiDiagramMapEntry *entries;
+  cl_uint sizeOfDiagram;
+} VoronoiDiagramMap;
+
+
+int get_hash(const VoronoiDiagramMap *map, const Coordinate *key) {
+  int result;
+  result = key->index;
+  return (result % map->sizeOfDiagram);
+}
+
+VoronoiDiagramMapEntry getVoronoiEntry(const VoronoiDiagramMap *map, const Coordinate *coord) {
+  return map->entries[get_hash(map, coord)];
+}
+
+void computeDistanceTransformImage(const UCImage *image, float *imageOutput) {
   static const float thresholdVal = 1.0f;
 
-  for(int x=0; x < imageWidth; ++x) {
-    for(int y=0; y < imageHeight; ++y) {
+  for(unsigned int x=0; x < image->attrs.width; ++x) {
+    for(unsigned int y=0; y < image->attrs.height; ++y) {
 
       float minDistance = std::numeric_limits<float>::max();
 
-      for(int ox=0; ox < imageWidth; ++ox) {
-        for(int oy=0; oy < imageHeight; ++oy) {
+      for(unsigned int ox=0; ox < image->attrs.width; ++ox) {
+        for(unsigned int oy=0; oy < image->attrs.height; ++oy) {
 
-          if(static_cast<float>(imageInput[imageHeight*oy + ox]) >= thresholdVal) {
+          if(static_cast<float>(image->image[image->attrs.height*oy + ox]) >= thresholdVal) {
             const float distance = std::sqrt( 
               static_cast<float>((ox-x)*(ox-x)) + 
               static_cast<float>((oy-y)*(oy-y)) );
@@ -32,16 +181,26 @@ void computeDistanceTransformImage(const unsigned char *imageInput, float *image
           }
         }
       }
-      imageOutput[imageHeight*y + x] = minDistance;
+      imageOutput[image->attrs.height*y + x] = minDistance;
     }
   }
 
 }
 
+unsigned char floatToPixVal(const float imageValue) {
+  int tmpval = static_cast<int>(::std::floor(256 * imageValue));
+  if (tmpval < 0) {
+      return 0u;
+  } else if (tmpval > 255) {
+      return 255u;
+  } else {
+      return tmpval & 0xffu;
+  }
+}
 
-class ExecuteOpenCL {
+class ExecuteDT {
 public:
-  ExecuteOpenCL(const std::string &filename)
+  ExecuteDT(const std::string &filename)
       : m_filename(filename), m_image(nullptr), m_output(nullptr){};
 
   void execute() {
@@ -53,25 +212,33 @@ public:
       throw std::runtime_error("The image could not be loaded, please check if "
                                "the filename is corrected");
 
+    const UCImage image = construcUCImage(m_image, imageHeight, imageWidth);
     const int imageSize = imageWidth * imageHeight;
     m_output = new float[imageSize];
     assert(m_output != nullptr);
 
     // Paralelo
-    OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteOpenCL::readKernel(), m_image,
-                               imageSize, m_output, imageSize);
+    //OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteOpenCL::readKernel(), m_image,
+                               //imageSize, m_output, imageSize);
 
     // Sequencial
-    //computeDistanceTransformImage(m_image, m_output, imageWidth, imageHeight);
-    for (int i = 0; i < imageSize; i++)
-      printf("%f ", m_output[i]);
-    printf("\n");
-    // A escrita na imagem de saída ainda deve ser consertada, esse método sempre
-    // espera um buffer de unsigned char...
-    stbi_write_bmp("result.bmp", imageWidth, imageHeight, 1, m_output);
+    computeDistanceTransformImage(&image, m_output);
+    // A imagem de saída para mostrar o resultado deve ser 
+    unsigned char *imageOut = new unsigned char[imageSize];
+    float maxValue = std::numeric_limits<float>::min();
+    for (int i = 0; i < imageSize; i++) {
+      if (maxValue < m_output[i])
+        maxValue = m_output[i];
+    }
+    for (int i = 0; i < imageSize; i++) {
+      imageOut[i] = floatToPixVal(m_output[i]/maxValue);
+    }
+    stbi_write_bmp("result.bmp", imageWidth, imageHeight, 1, imageOut);
+
+    free(imageOut);
   }
 
-  ~ExecuteOpenCL() {
+  ~ExecuteDT() {
     if (m_image != nullptr)
       stbi_image_free(m_image);
 
@@ -108,7 +275,7 @@ int main(int argc, char const *argv[]) {
 
   try {
     // Executa com o destrutor seguro para desalocar todos os ponteiros criados.
-    ExecuteOpenCL exec(filename);
+    ExecuteDT exec(filename);
     exec.execute();
   } catch (const std::runtime_error &e) {
     throw e;
