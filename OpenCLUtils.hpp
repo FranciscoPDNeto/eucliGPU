dcf79626a644b0dcfb8e0062a788d6c56103a9c5
Francisco Dias <franciscopdneto@gmail.com>
2019-11-19 21:25:33 -0300

    put image functions and structures into ImageUtils

--- OpenCLUtils.hpp
+++ OpenCLUtils.hpp
@@ -1,10 +1,86 @@
 #include <iostream>
 #include <stdexcept>
 
-#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
-#include <CL/cl.hpp>
+#include "ImageUtils.hpp"
 
 namespace OpenCLUtils {
+
+const char *getErrorString(cl_int error)
+{
+switch(error){
+    // run-time and JIT compiler errors
+    case 0: return "CL_SUCCESS";
+    case -1: return "CL_DEVICE_NOT_FOUND";
+    case -2: return "CL_DEVICE_NOT_AVAILABLE";
+    case -3: return "CL_COMPILER_NOT_AVAILABLE";
+    case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+    case -5: return "CL_OUT_OF_RESOURCES";
+    case -6: return "CL_OUT_OF_HOST_MEMORY";
+    case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
+    case -8: return "CL_MEM_COPY_OVERLAP";
+    case -9: return "CL_IMAGE_FORMAT_MISMATCH";
+    case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+    case -11: return "CL_BUILD_PROGRAM_FAILURE";
+    case -12: return "CL_MAP_FAILURE";
+    case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+    case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+    case -15: return "CL_COMPILE_PROGRAM_FAILURE";
+    case -16: return "CL_LINKER_NOT_AVAILABLE";
+    case -17: return "CL_LINK_PROGRAM_FAILURE";
+    case -18: return "CL_DEVICE_PARTITION_FAILED";
+    case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+    // compile-time errors
+    case -30: return "CL_INVALID_VALUE";
+    case -31: return "CL_INVALID_DEVICE_TYPE";
+    case -32: return "CL_INVALID_PLATFORM";
+    case -33: return "CL_INVALID_DEVICE";
+    case -34: return "CL_INVALID_CONTEXT";
+    case -35: return "CL_INVALID_QUEUE_PROPERTIES";
+    case -36: return "CL_INVALID_COMMAND_QUEUE";
+    case -37: return "CL_INVALID_HOST_PTR";
+    case -38: return "CL_INVALID_MEM_OBJECT";
+    case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+    case -40: return "CL_INVALID_IMAGE_SIZE";
+    case -41: return "CL_INVALID_SAMPLER";
+    case -42: return "CL_INVALID_BINARY";
+    case -43: return "CL_INVALID_BUILD_OPTIONS";
+    case -44: return "CL_INVALID_PROGRAM";
+    case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
+    case -46: return "CL_INVALID_KERNEL_NAME";
+    case -47: return "CL_INVALID_KERNEL_DEFINITION";
+    case -48: return "CL_INVALID_KERNEL";
+    case -49: return "CL_INVALID_ARG_INDEX";
+    case -50: return "CL_INVALID_ARG_VALUE";
+    case -51: return "CL_INVALID_ARG_SIZE";
+    case -52: return "CL_INVALID_KERNEL_ARGS";
+    case -53: return "CL_INVALID_WORK_DIMENSION";
+    case -54: return "CL_INVALID_WORK_GROUP_SIZE";
+    case -55: return "CL_INVALID_WORK_ITEM_SIZE";
+    case -56: return "CL_INVALID_GLOBAL_OFFSET";
+    case -57: return "CL_INVALID_EVENT_WAIT_LIST";
+    case -58: return "CL_INVALID_EVENT";
+    case -59: return "CL_INVALID_OPERATION";
+    case -60: return "CL_INVALID_GL_OBJECT";
+    case -61: return "CL_INVALID_BUFFER_SIZE";
+    case -62: return "CL_INVALID_MIP_LEVEL";
+    case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
+    case -64: return "CL_INVALID_PROPERTY";
+    case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
+    case -66: return "CL_INVALID_COMPILER_OPTIONS";
+    case -67: return "CL_INVALID_LINKER_OPTIONS";
+    case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+    // extension errors
+    case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+    case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
+    case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
+    case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
+    case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+    case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+    default: return "Unknown OpenCL error";
+    }
+}
 
 std::vector<cl::Device> getDevices() {
   std::vector<cl::Platform> platforms;
@@ -63,8 +139,8 @@ cl::Device userSelectDevice() {
 
 void executeOpenCL(const std::string &kernelName,
                    const std::string &kernelSource,
-                   const unsigned char *dataInput, const int dataInputSize,
-                   float *dataOutput, const int dataOutputSize) {
+                   const UCImage *image,
+                   cl_float *dataOutput) {
 
   const cl::Device defaultDevice = getDevice(0);
 
@@ -79,30 +155,40 @@ void executeOpenCL(const std::string &kernelName,
         program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(defaultDevice));
   }
 
-  // Estudando alterar para Image2D
-  /*
-  cl::ImageFormat format(CL_INTENSITY, CL_UNORM_INT8);
-  cl::Image2D image(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, format, imageWidth, 
-    imageHeight, imageWidth * sizeof(unsigned char), image);
-  */
   cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR,
-                         sizeof(unsigned char) * dataInputSize, nullptr);
+                         sizeof(cl_uchar)*image->attrs.size, nullptr);
+  cl::Buffer inputAttrsBuffer(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR,
+                         sizeof(ImageAttrs), nullptr);
   cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR,
-                          sizeof(float) * dataOutputSize, nullptr);
+                          sizeof(cl_float) * image->attrs.size, nullptr);
 
   cl::CommandQueue queue(context, defaultDevice);
-  queue.enqueueWriteBuffer(inputBuffer, CL_TRUE, 0,
-                           sizeof(unsigned char) * dataInputSize, dataInput);
+  
+  cl_int errorCode = queue.enqueueWriteBuffer(inputBuffer, CL_TRUE, 0,
+                           sizeof(cl_uchar)*image->attrs.size, image->image);
+  if (errorCode != CL_SUCCESS)
+    throw std::runtime_error(getErrorString(errorCode));
+
+  errorCode = queue.enqueueWriteBuffer(inputAttrsBuffer, CL_TRUE, 0,
+                           sizeof(ImageAttrs), &(image->attrs));
+  if (errorCode != CL_SUCCESS)
+    throw std::runtime_error(getErrorString(errorCode));
+  
 
   cl::Kernel kernel(program, kernelName.c_str());
   kernel.setArg(0, inputBuffer);
-  kernel.setArg(1, outputBuffer);
+  kernel.setArg(1, inputAttrsBuffer);
+  kernel.setArg(2, outputBuffer);
 
-  queue.enqueueNDRangeKernel(kernel, 0, dataInputSize, 32);
+  errorCode = queue.enqueueNDRangeKernel(kernel, 0, image->attrs.size, 32);
+  if (errorCode != CL_SUCCESS)
+    throw std::runtime_error(getErrorString(errorCode));
 
   // Retorna o resultado da computação na GPU para o dataOutput.
-  queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0,
-                          sizeof(float) * dataOutputSize, dataOutput);
+  errorCode = queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0,
+                          sizeof(cl_float) * image->attrs.size, dataOutput);
+  if (errorCode != CL_SUCCESS)
+    throw std::runtime_error(getErrorString(errorCode));
 }
 
 } // namespace OpenCLUtils
--- eucligpu.cpp
+++ eucligpu.cpp
@@ -9,157 +9,6 @@
 
 #define KERNELNAME "euclidean"
 
-/**
- * \brief Representação de coordenada 2d.
-*/
-typedef struct {
-  cl_uint index;
-  cl_uint y;
-  cl_uint x;
-
-} Coordinate;
-
-Coordinate constructCoord(unsigned int y, unsigned int x, unsigned int imageWidth) {
-  Coordinate coord;
-  coord.index = y*imageWidth + x;
-  coord.y = y;
-  coord.x = x;
-
-  return coord;
-}
-
-/**
- * \brief Calcula a distância euclideana.
-*/
-cl_float euclideanDistance(const Coordinate& coord1, const Coordinate& coord2) {
-  return std::sqrt(std::pow(coord1.y - coord2.y, 2) + std::pow(coord1.x - coord2.x, 2));
-}
-
-/**
- * \brief Representa o Pixel da imagem.
-*/
-typedef struct {
-  Coordinate coord;
-  // Valor que corresponde a true caso seja célula de fundo, ou false(0) caso seja célula de frente(1).
-  bool background;
-} Pixel;
-
-Pixel constructPixel(const Coordinate coord, const bool background) {
-  Pixel pixel;
-  pixel.coord = coord;
-  pixel.background = background;
-  
-  return pixel;
-}
-
-typedef struct {
-  // Há no máximo 8 vizinhos.
-  Pixel pixels[8];
-  // É unsigned char pois pode representar no máximo 8, então não é necessário
-  // mais de um byte.
-  cl_uchar size;
-  
-} Neighborhood;
-
-Neighborhood initNeighborhood() {
-  Neighborhood neighborhood;
-  neighborhood.size = 0;
-
-  return neighborhood;
-}
-
-void addNeighbor(Neighborhood neighborhood, Pixel pixel) {
-  neighborhood.pixels[neighborhood.size++] = pixel;
-}
-
-Pixel getNeighbor(const Neighborhood neighborhood, const unsigned char index) {
-  assert(index < neighborhood.size);
-  const Pixel pixel = neighborhood.pixels[index];
-
-  return pixel;
-}
-
-typedef struct {
-  cl_uint height;
-  cl_uint width;
-  cl_uint size;
-} ImageAttrs;
-
-typedef struct {
-  ImageAttrs attrs; 
-  cl_uchar *image;
-} UCImage;
-
-
-UCImage construcUCImage(unsigned char *image, const unsigned int height, const unsigned int width) {
-  UCImage ucimage;
-  ucimage.image = image;
-  ucimage.attrs.height = height;
-  ucimage.attrs.width = width;
-  ucimage.attrs.size = width * height;
-
-  return ucimage;
-}
-
-cl_uchar getValueByCoord(const UCImage *image, const Coordinate coord) {
-  return image->image[coord.y * image->attrs.width + coord.x];
-}
-
-bool isBackgroudByCoord(const UCImage *image, const Coordinate coord) {
-  return getValueByCoord(image, coord) == 0;
-}
-
-Pixel getPixel(const UCImage *image, cl_int y, cl_int x) {
-  const Coordinate coord = constructCoord(y, x, image->attrs.width);
-  return constructPixel(coord, isBackgroudByCoord(image, coord));
-}
-
-/**
- * \brief Retorna a lista de valores vizinhos ao pixel na imagem.
- * A vizinhança utilizada é a simples, vizinhança direta na janela 3x3.
-*/
-Neighborhood getNeighborhood(const UCImage *image, const Pixel& pixel) {
-  Neighborhood neighborhood;
-  for (cl_int i = -1; i < 2; i++) {
-    const cl_uint y = pixel.coord.y - i;
-    if (y >= image->attrs.height)
-      continue;
-
-    for (cl_int j = -1; i < 2; i++) {
-      if (j == 0 && i == 0)
-        continue;
-      const cl_uint x = pixel.coord.x - j;
-      if (x < 0 || x >= image->attrs.width)
-        continue;
-
-
-      addNeighbor(neighborhood, getPixel(image, y, x));
-    }
-  }
-  
-  return neighborhood;
-}
-
-typedef struct {
-  Coordinate point;
-  Coordinate nearestBackground;
-} VoronoiDiagramMapEntry;
-
-typedef struct {
-  VoronoiDiagramMapEntry *entries;
-  cl_uint sizeOfDiagram;
-} VoronoiDiagramMap;
-
-
-int get_hash(const VoronoiDiagramMap *map, const Coordinate *key) {
-  int result;
-  result = key->index;
-  return (result % map->sizeOfDiagram);
-}
-
-VoronoiDiagramMapEntry getVoronoiEntry(const VoronoiDiagramMap *map, const Coordinate *coord) {
-  return map->entries[get_hash(map, coord)];
-}
 
 void computeDistanceTransformImage(const UCImage *image, float *imageOutput) {
   static const float thresholdVal = 1.0f;
@@ -220,11 +69,11 @@ public:
     assert(m_output != nullptr);
 
     // Paralelo
-    //OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteOpenCL::readKernel(), m_image,
-                               //imageSize, m_output, imageSize);
+    OpenCLUtils::executeOpenCL(KERNELNAME, ExecuteDT::readKernel(), &image,
+                               m_output);
 
     // Sequencial
-    computeDistanceTransformImage(&image, m_output);
+    //computeDistanceTransformImage(&image, m_output);
     // A imagem de saída para mostrar o resultado deve ser 
     unsigned char *imageOut = new unsigned char[imageSize];
     float maxValue = std::numeric_limits<float>::min();
